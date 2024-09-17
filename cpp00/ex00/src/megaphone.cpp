868b1252b2659494c38b3d4504c963bb69d83ae3
theeunsu <theeunsu@gmail.com>
2024-09-17 23:30:52 +0200

    cpp and makefile

---
+++ cpp00/ex00/src/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 22:47:07 by eahn              #+#    #+#             */
+/*   Updated: 2024/09/17 23:28:08 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
