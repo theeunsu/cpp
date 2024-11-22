fb28c8645a3f4b682895e2ed81c0749d0686408e
theeunsu <theeunsu@gmail.com>
2024-11-22 22:06:31 +0100

    animal

---
+++ cpp04/ex00/src/Animal.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 22:01:10 by eahn              #+#    #+#             */
+/*   Updated: 2024/11/22 22:06:02 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Animal.hpp"
+
+Animal::Animal() : type("default")
+{
+	std::cout << "[Animal] Default Constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other) : type(other.type)
+{
+	std::cout << "[Animal] Copy Constructor called" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+	if (this != &other)
+	{
+		type = other.type;
+		std::cout << "[Animal] Assignment Operator called" << std::endl;
+	}
+	return *this;
+}
+
+Animal::~Animal()
+{
+	std::cout << "[Animal] Destructor called" << std::endl;
+}
+
+std::string Animal::getType(void) const
+{
+	return type;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "[Animal] Generic sound" << std::endl;
+}
