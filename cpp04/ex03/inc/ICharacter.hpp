8a797ecf7ef49fe1d1bd66b8affc789b4c9bdd77
theeunsu <theeunsu@gmail.com>
2024-11-25 14:29:31 +0100

    final

--- cpp04/ex03/inc/ICharacter.hpp
+++ cpp04/ex03/inc/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 22:18:34 by eahn              #+#    #+#             */
-/*   Updated: 2024/11/24 22:48:33 by eahn             ###   ########.fr       */
+/*   Updated: 2024/11/25 14:01:35 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ class AMateria;
 
 class ICharacter
 {
-public:
-virtual ~ICharacter() {}
-virtual std::string const & getName() const = 0; // Returns the character's name
-virtual void equip(AMateria* m) = 0;
-virtual void unequip(int idx) = 0;
-virtual void use(int idx, ICharacter& target) = 0; 
+    public:
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0; // Returns the character's name
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0; 
 };
 
 #endif
